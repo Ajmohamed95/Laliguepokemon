d75884ccc2f7bd7d2cbda6b5b5e9b4adfa557bb9
Ajmohamed95 <140144@student.ichec.be>
2021-04-19 22:00:46 +0200

    Vue.js

---
+++ liguepokemon_vuejs/src/router.js
@@ -0,0 +1,17 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+import login from "./views/login.vue";
+
+
+
+Vue.use(VueRouter)
+export default new VueRouter({ 
+ routes : [
+  {
+    path: "/login",
+    component: login
+  },
+  
+]
+})
