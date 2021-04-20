6fc67577b4f1f086a7036ea1594f89cf768bb012
Ajmohamed95 <140144@student.ichec.be>
2021-04-20 20:35:15 +0200

    vuejs

--- App.js
+++ App.js
@@ -19,6 +19,15 @@ app.use(session({
     saveUninitialized : false
 })
 );
+app.all('/*', function(req, res, next) {
+    // CORS headers
+    res.header("Access-Control-Allow-Origin", "*"); // restrictitto the requireddomain
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
+    if(req.method== 'OPTIONS') {res.status(200).end();
+    } 
+    else { next(); }
+  });
 
 // app.get('/',(req,res)=> res.render('todo.ejs',{listnom:listnom,message:""}))
 // //app.post('/add',(req,res)=> {let newList = req.query.newList;listnom.push(newList);res.render('todo.ejs',{listnom:listnom});})
